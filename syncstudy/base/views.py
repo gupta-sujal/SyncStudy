c26a8910f64fff77360f8901c8c142733f4b34f1
gupta-sujal <sujalgupta484@gmail.com>
2023-12-26 18:28:23 +0530

    added html templating and created in-app templates

--- syncstudy/base/views.py
+++ syncstudy/base/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    return render(request,'home.html')
+rooms=[
+    {'id':1,'name':'learning django'},
+    { 'id':2, 'name':'learning python'},
+    { 'id':3,'name':'designing UI/UX'},
+]
 
-def room(request):
-    return render(request,'room.html')
+def home(request):
+    return render(request,'base/home.html',{'rooms':rooms})
+
+def room(request,pk):
+    room=None
+    for i in rooms:
+        if i['id']==int(pk):
+            room=i
+    context={'room':room}
+
+    return render(request,'base/room.html',context)
 
