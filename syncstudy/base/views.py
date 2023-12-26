028c628fca26f02942a2a6d6ee71d5da2712d7ae
gupta-sujal <sujalgupta484@gmail.com>
2023-12-26 19:36:46 +0530

    added the User, Room, Message, Topic models and rendered some more info on the home page

--- syncstudy/base/views.py
+++ syncstudy/base/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Room
 rooms=[
     {'id':1,'name':'learning django'},
     { 'id':2, 'name':'learning python'},
@@ -8,13 +8,11 @@ rooms=[
 ]
 
 def home(request):
+    rooms=Room.objects.all()
     return render(request,'base/home.html',{'rooms':rooms})
 
 def room(request,pk):
-    room=None
-    for i in rooms:
-        if i['id']==int(pk):
-            room=i
+    room=Room.objects.get(id=pk)
     context={'room':room}
 
     return render(request,'base/room.html',context)
