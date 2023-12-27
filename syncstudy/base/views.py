5ac4a0d81b8b3e9ad9a77bb054761a2014dfda80
gupta-sujal <sujalgupta484@gmail.com>
2023-12-27 20:04:41 +0530

    added the forms to create, update, delete rooms , all the CRUD operations

--- syncstudy/base/views.py
+++ syncstudy/base/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Room
+from .forms import RoomForm
+
 rooms=[
     {'id':1,'name':'learning django'},
     { 'id':2, 'name':'learning python'},
@@ -16,4 +18,38 @@ def room(request,pk):
     context={'room':room}
 
     return render(request,'base/room.html',context)
+
+def create_room(request):
+    form=RoomForm()
+    if request.method=='POST':#if the request is of POST type
+        form=RoomForm(request.POST)
+        if(form.is_valid):# if the form is valid
+            form.save()
+            return redirect('home')
+    context={'form':form}
+    return render(request, 'base/room_form.html', context)
+
+def updateRoom(request,pk):
+    room=Room.objects.get(id=pk)
+
+    form=RoomForm(instance=room)
+    if request.method=='POST':#if the request is of POST type
+        form=RoomForm(request.POST, instance=room)# add the instance to tell which room to update
+        if(form.is_valid):# if the form is valid
+            form.save()
+            return redirect('home')
+    context={'form':form}
+    return render(request, 'base/room_form.html', context)
+
+
+def deleteRoom(request,pk):
+    room=Room.objects.get(id=pk)
+    context={'obj':room}
+    if(request.method == 'POST'):
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', context)
+
+
+
 
