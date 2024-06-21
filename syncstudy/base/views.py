35d21df904fd138ee04c338f1752827b72deca30
gupta-sujal <sujalgupta484@gmail.com>
2024-06-22 00:10:22 +0530

    retrieve the messages inside a room and display them

--- syncstudy/base/views.py
+++ syncstudy/base/views.py
@@ -72,8 +72,9 @@ def home(request):
 
 def room(request,pk):
     room=Room.objects.get(id=pk)
-    context={'room':room}
-
+    # getting all the children of the room (i.e. the messages)
+    room_messages=room.message_set.all().order_by('-created')
+    context={'room':room,'room_messages':room_messages}
     return render(request,'base/room.html',context)
 
 
